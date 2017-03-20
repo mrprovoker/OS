156919f39b1cf5ace204704455812bef688c73ab
Provoker <groser95@gmail.com>
2017-03-20 09:56:21 +0500

    minor fix

--- TASK2/sp_prog.c
+++ TASK2/sp_prog.c
@@ -19,58 +19,58 @@ const char* errorDesc[] =
 };
 
 
-int printError(ErrorCode c) 
+int printError(ErrorCode c)
 {
     fprintf(stderr, "%s", errorDesc[c]);
     exit(1);
 }
 
 // Получить дескриптор файла, если можно
-int getFileDescriptor(char *name) 
+int getFileDescriptor(char *name)
 {
-	int fd = open(name, O_WRONLY|O_EXCL|O_CREAT, 0666);
-	return (fd < 0) ? printError(OPEN_ERR) : fd;
+    int fd = open(name, O_WRONLY|O_EXCL|O_CREAT, 0666);
+    return (fd < 0) ? printError(OPEN_ERR) : fd;
 }
 
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    if (argc < 2) 
-	{
+    if (argc < 2)
+    {
         printError(ARG_MISS);
     };
 
     int fd = getFileDescriptor(argv[1]);
     char byte;
 
-    // Считываем файл из stdin 
-    while (read(STDIN_FILENO, &byte, 1) > 0) 
-	{
-        if (byte == 0) 
-		{
+    // Считываем файл из stdin
+    while (read(STDIN_FILENO, &byte, 1) > 0)
+    {
+        if (byte == 0)
+        {
             // Сдвиг, если нулевой байт
-            if (lseek(fd, 1, SEEK_CUR) <= 0) 
-			{
+            if (lseek(fd, 1, SEEK_CUR) <= 0)
+            {
                 printError(WRITE_ERR);
             };
-			continue;
+            continue;
         }
-		
-		if (write(fd, &byte, 1) <= 0) 
-		{
-			printError(WRITE_ERR);
-		}; 
-    };
 
-    // Если файл состоит из нулей, нужно хотя бы одним нулевым байтом забить 
-    if (byte == 0) 
-	{
-        if (lseek(fd, -1, SEEK_CUR) <= 0) 
-		{
+        if (write(fd, &byte, 1) <= 0)
+        {
             printError(WRITE_ERR);
         };
-        if (write(fd, &byte, 1) <= 0) 
-		{
+    };
+
+    // Если файл состоит из нулей, нужно хотя бы одним нулевым байтом забить
+    if (byte == 0)
+    {
+        if (lseek(fd, -1, SEEK_CUR) <= 0)
+        {
+            printError(WRITE_ERR);
+        };
+        if (write(fd, &byte, 1) <= 0)
+        {
             printError(WRITE_ERR);
         };
     };
