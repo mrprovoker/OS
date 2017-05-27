1f6f1e9cb2278a10d2f49c6e7b8daece0571b155
Provoker <groser95@gmail.com>
2017-05-27 21:34:23 +0500

    Отступы

--- TASK6/os6.c
+++ TASK6/os6.c
@@ -12,9 +12,9 @@ typedef enum
 {
     ARG_MISS=0,
     LOCK_WRITE_ERR,
-	OPEN_ERR,
-	WRITE_ERR,
-	RMV_LOCK_ERR
+    OPEN_ERR,
+    WRITE_ERR,
+    RMV_LOCK_ERR
 } ErrorCode;
 
 // Текстовое содержание ошибок
@@ -37,25 +37,25 @@ FILE* printError(ErrorCode c)
 // Получить полное имя файла блокировки
 char* getLockerName(char* name) 
 {
-	static char path[100];
-	strcpy(path, name);
-	strcat(path, ".lck");
-	
-	return path;
+    static char path[100];
+    strcpy(path, name);
+    strcat(path, ".lck");
+    
+    return path;
 }
 
 // Подождать пока файл освободится и создать блокировку
 void waitAndLockFile(char *locker_name) 
 {
-	while(1) 
-	{
-		int fd = open(locker_name, O_WRONLY|O_CREAT|O_EXCL, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+    while(1) 
+    {
+        int fd = open(locker_name, O_WRONLY|O_CREAT|O_EXCL, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
         if (fd >= 0) 
-		{
+        {
             pid_t pid = getpid();
             if (dprintf(fd, "W%d\n", pid) < 0) 
-			{
-				printError(LOCK_WRITE_ERR);
+            {
+                printError(LOCK_WRITE_ERR);
             };
             close(fd);
             break;
@@ -65,32 +65,32 @@ void waitAndLockFile(char *locker_name)
 
 
 int main(int argc, char *argv[]) 
-{	
-	if (argc != 3)
+{    
+    if (argc != 3)
     {
         printError(ARG_MISS);
     };
-	
-	char* locker_name = getLockerName(argv[1]);  
-	waitAndLockFile(locker_name);
-	
+    
+    char* locker_name = getLockerName(argv[1]);  
+    waitAndLockFile(locker_name);
+    
     FILE *fp = fopen(argv[1], "a");
     if (fp == NULL) 
-	{
-		printError(OPEN_ERR);
+    {
+        printError(OPEN_ERR);
     };
-	
-	sleep(SLEEP_TIME);  // создать видимость работы с файлом
-	
+    
+    sleep(SLEEP_TIME);  // создать видимость работы с файлом
+    
     if (fprintf(fp, "%s\n", argv[2]) < 0) 
-	{
-		printError(WRITE_ERR);
+    {
+        printError(WRITE_ERR);
     };
-	fclose(fp);
-	
+    fclose(fp);
+    
     if (remove(locker_name) < 0) 
-	{
-		printError(RMV_LOCK_ERR);
+    {
+        printError(RMV_LOCK_ERR);
     };
 
     return 0;
